38f385ea1efff4342ede1318712f954f66eaa5aa
YH.Jang <YoungheeJang@gmail.com>
2019-01-09 14:57:41 +0900

    setting

---
+++ JYHPageControl/TestPageControl.swift
@@ -0,0 +1,15 @@
+//
+//  TestPageControl.swift
+//  JYHPageControl
+//
+//  Created by YoungheeJang on 09/01/2019.
+//  Copyright © 2019 YH.Jang. All rights reserved.
+//
+
+import UIKit
+
+class TestPageControl: UIPageControl {
+    func text() {
+        print("xxxX")
+    }
+}
