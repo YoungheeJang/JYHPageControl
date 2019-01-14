b6c6080420e169d6101da3c3d882181c72b6a158
YH.Jang <YoungheeJang@gmail.com>
2019-01-14 13:52:33 +0900

    add sample image resource

---
+++ JYHPageControlSample/JYHPageControlSample/pageCopntrol.swift
@@ -0,0 +1,21 @@
+//
+//  pageCopntrol.swift
+//  JYHPageControlSample
+//
+//  Created by YoungheeJang on 11/01/2019.
+//  Copyright © 2019 Younghee Jang. All rights reserved.
+//
+
+import UIKit
+
+class pageCopntrol: UIPageControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
