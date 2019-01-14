e57d8844d64d0bd9b8a6865f365f8eaba398f22e
YH.Jang <YoungheeJang@gmail.com>
2019-01-14 13:54:03 +0900

    update jyh and sample

--- JYHPageControl/JYHPageControl.swift
+++ JYHPageControl/JYHPageControl.swift
@@ -8,8 +8,182 @@
 
 import UIKit
 
-open class JYHPageControl: UIPageControl {
-    func text() {
-        print("xxxX")
+open class JYHPageControl: UIView {
+    fileprivate var currentDotSize: CGSize!
+    fileprivate var currentDotBackgroundColor: UIColor!
+    fileprivate var currentDotBorderColor: UIColor!
+    fileprivate var currentDotAlpha: CGFloat!
+    
+    fileprivate var defaultDotSize: CGSize!
+    fileprivate var defaultDotBackgroundColor: UIColor!
+    fileprivate var defaultDotBorderColor: UIColor!
+    fileprivate var defaultDotAlpha: CGFloat!
+    
+    fileprivate var gab: CGFloat!
+    fileprivate var radius: Bool!
+    
+    fileprivate var headerImageView: UIImageView?
+    fileprivate var footerImageView: UIImageView?
+    
+    fileprivate var numberOfPages: Int!
+    
+    open var currentPage: Int! {
+        didSet {
+            if dotArray.count > 0 {
+                UIView.animate(withDuration: 0.3) {
+                    self.updateView()
+                }
+            }
+        }
     }
+    fileprivate var dotArray: [PageControlDot] = []
+    
+    public convenience init(currentDotSize: CGSize = CGSize(width: 6, height: 6),
+                            currentDotBackgroundColor: UIColor = UIColor.white,
+                            currentDotBorderColor: UIColor = UIColor.lightGray,
+                            currentDotAlpha: CGFloat = 1.0,
+                            
+                            defaultDotSize: CGSize = CGSize(width: 6, height: 6),
+                            defaultDotBackgroundColor: UIColor = UIColor.white,
+                            defaultDotBorderColor: UIColor = UIColor.lightGray,
+                            defaultDotAlpha: CGFloat = 0.7,
+                            
+                            gab: CGFloat = 6,
+                            radius: Bool = true,
+                            
+                            numberOfPages: Int,
+                            currentPage: Int = 0) {
+        self.init(frame: .zero)
+        self.currentDotSize = currentDotSize
+        self.currentDotBackgroundColor = currentDotBackgroundColor
+        self.currentDotBorderColor = currentDotBorderColor
+        self.currentDotAlpha = currentDotAlpha
+        
+        self.defaultDotSize = defaultDotSize
+        self.defaultDotBackgroundColor = defaultDotBackgroundColor
+        self.defaultDotBorderColor = defaultDotBorderColor
+        self.defaultDotAlpha = defaultDotAlpha
+        
+        self.gab = gab
+        self.radius = radius
+        
+        self.numberOfPages = numberOfPages
+        self.currentPage = currentPage
+        
+        self.updateNumberOfPages()
+    }
+    
+    public func useHeaderImageView(size: CGSize, image: UIImage) {
+        if self.headerImageView == nil {
+            self.headerImageView = UIImageView()
+        }
+        self.headerImageView?.image = image
+        self.headerImageView?.contentMode = .scaleAspectFit
+        self.headerImageView?.clipsToBounds = true
+        self.headerImageView?.frame = CGRect(origin: .zero, size: size)
+        self.addSubview(self.headerImageView!)
+    }
+    
+    public func removeHeaderImageView() {
+        if self.headerImageView != nil {
+            self.headerImageView?.removeFromSuperview()
+            self.headerImageView = nil
+        }
+    }
+    
+    public func useFooterImageView(size: CGSize, image: UIImage) {
+        if self.footerImageView == nil {
+            self.footerImageView = UIImageView()
+        }
+        self.footerImageView?.image = image
+        self.footerImageView?.contentMode = .scaleAspectFit
+        self.footerImageView?.clipsToBounds = true
+        self.footerImageView?.frame = CGRect(origin: .zero, size: size)
+        self.addSubview(self.footerImageView!)
+    }
+    
+    public func removeFooterImageView() {
+        if self.footerImageView != nil {
+            self.footerImageView?.removeFromSuperview()
+            self.footerImageView = nil
+        }
+    }
+    
+    override open func draw(_ rect: CGRect) {
+        self.updateView()
+    }
+    
+    private func updateView() {
+        self.updateDots()
+        self.updateHeaderImageView()
+        self.updateFooterImageView()
+    }
+    
+    private func updateDots() {
+        guard let currentDotSize = self.currentDotSize else { return }
+        guard let defaultDotSize = self.defaultDotSize else { return }
+        
+        let subviewsWidth = currentDotSize.width + defaultDotSize.width * CGFloat((numberOfPages - 1)) + gab * CGFloat((numberOfPages - 1))
+        var x = (self.frame.size.width - subviewsWidth) / 2
+        
+        for (i, view) in dotArray.enumerated() {
+            let width =             i == currentPage ? currentDotSize.width : defaultDotSize.width
+            let height =            i == currentPage ? currentDotSize.height : defaultDotSize.width
+            let alpha: CGFloat =    i == currentPage ? currentDotAlpha : defaultDotAlpha
+            let backgroundColor =   i == currentPage ? currentDotBackgroundColor : defaultDotBackgroundColor
+            let borderColor =       i == currentPage ? currentDotBorderColor.cgColor : defaultDotBorderColor.cgColor
+            let y = (self.frame.size.height - height) / 2
+            
+            view.frame = CGRect(x: x, y: y, width: width, height: height)
+            view.alpha = alpha
+            view.backgroundColor = backgroundColor
+            view.layer.borderColor = borderColor
+            view.layer.borderWidth = 1
+            if radius { view.layer.cornerRadius = height / 2 }
+            
+            x = x + gab + width
+        }
+    }
+    
+    private func updateHeaderImageView() {
+        guard let imageView = self.headerImageView else { return }
+        guard let frontView = dotArray.first else { return }
+        
+        let x = frontView.frame.origin.x - gab - imageView.frame.size.width
+        let y = (self.frame.height - imageView.frame.height) / 2
+        
+        imageView.frame = CGRect(x: x, y: y, width: imageView.frame.size.width, height: imageView.frame.size.height)
+    }
+    
+    private func updateFooterImageView() {
+        guard let imageView = self.footerImageView else { return }
+        guard let frontView = dotArray.last else { return }
+        
+        let x = frontView.frame.origin.x + frontView.frame.size.width + gab
+        let y = (self.frame.height - imageView.frame.height) / 2
+        
+        imageView.frame = CGRect(x: x, y: y, width: imageView.frame.size.width, height: imageView.frame.size.height)
+    }
+    
+    private func updateNumberOfPages() {
+        for view in dotArray {
+            view.removeFromSuperview()
+        }
+        dotArray.removeAll()
+        
+        for _ in 0..<self.numberOfPages {
+            let view = PageControlDot()
+            self.addSubview(view)
+            dotArray.append(view)
+        }
+    }
+}
+
+class PageControlDot: UIView {
+    
+    convenience init() {
+        self.init(frame: .zero)
+        self.clipsToBounds = true
+    }
+    
 }
--- JYHPageControlSample/JYHPageControlSample/ViewController.swift
+++ JYHPageControlSample/JYHPageControlSample/ViewController.swift
@@ -7,12 +7,143 @@
 //
 
 import UIKit
+import JYHPageControl
 
 class ViewController: UIViewController {
-
+  
+  var scrollView: UIScrollView?
+  var pageControl: JYHPageControl?
+  
+  let sampleImage01 = UIImageView(image: UIImage(named: "sample01"))
+  let sampleImage02 = UIImageView(image: UIImage(named: "sample02"))
+  let sampleImage03 = UIImageView(image: UIImage(named: "sample03"))
+  let sampleImage04 = UIImageView(image: UIImage(named: "sample04"))
+  let sampleImage05 = UIImageView(image: UIImage(named: "sample05"))
+  let sampleImage06 = UIImageView(image: UIImage(named: "sample06"))
+  let sampleImage07 = UIImageView(image: UIImage(named: "sample07"))
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    scrollView = UIScrollView()
+    scrollView?.isPagingEnabled = true
+    scrollView?.isScrollEnabled = true
+    scrollView?.showsVerticalScrollIndicator = false
+    scrollView?.showsHorizontalScrollIndicator = false
+    scrollView?.translatesAutoresizingMaskIntoConstraints = false
+    scrollView?.delegate = self
+    
+    pageControl = JYHPageControl(currentDotSize: CGSize(width: 20, height: 12),
+                                 currentDotBackgroundColor: UIColor.red,
+                                 currentDotBorderColor: UIColor.lightGray,
+                                 currentDotAlpha: 1.0,
+                                 defaultDotSize: CGSize(width: 6, height: 6),
+                                 defaultDotBackgroundColor: UIColor.white,
+                                 defaultDotBorderColor: UIColor.lightGray,
+                                 defaultDotAlpha: 0.7,
+                                 gab: 6,
+                                 radius: true,
+                                 numberOfPages: 7,
+                                 currentPage: 0)
+    
+    pageControl?.useHeaderImageView(size: CGSize(width: 30, height: 30), image: UIImage(named: "sampleHeader")!)
+    pageControl?.useFooterImageView(size: CGSize(width: 30, height: 30), image: UIImage(named: "sampleFooter")!)
+    pageControl?.translatesAutoresizingMaskIntoConstraints = false
+    
+    sampleImage01.contentMode = .scaleAspectFill
+    sampleImage01.clipsToBounds = true
+    sampleImage02.contentMode = .scaleAspectFill
+    sampleImage02.clipsToBounds = true
+    sampleImage03.contentMode = .scaleAspectFill
+    sampleImage03.clipsToBounds = true
+    sampleImage04.contentMode = .scaleAspectFill
+    sampleImage04.clipsToBounds = true
+    sampleImage05.contentMode = .scaleAspectFill
+    sampleImage05.clipsToBounds = true
+    sampleImage06.contentMode = .scaleAspectFill
+    sampleImage06.clipsToBounds = true
+    sampleImage07.contentMode = .scaleAspectFill
+    sampleImage07.clipsToBounds = true
+    
+    sampleImage01.translatesAutoresizingMaskIntoConstraints = false
+    sampleImage02.translatesAutoresizingMaskIntoConstraints = false
+    sampleImage03.translatesAutoresizingMaskIntoConstraints = false
+    sampleImage04.translatesAutoresizingMaskIntoConstraints = false
+    sampleImage05.translatesAutoresizingMaskIntoConstraints = false
+    sampleImage06.translatesAutoresizingMaskIntoConstraints = false
+    sampleImage07.translatesAutoresizingMaskIntoConstraints = false
+  
+    self.view.addSubview(scrollView!)
+    self.view.addSubview(pageControl!)
+    self.scrollView?.addSubview(sampleImage01)
+    self.scrollView?.addSubview(sampleImage02)
+    self.scrollView?.addSubview(sampleImage03)
+    self.scrollView?.addSubview(sampleImage04)
+    self.scrollView?.addSubview(sampleImage05)
+    self.scrollView?.addSubview(sampleImage06)
+    self.scrollView?.addSubview(sampleImage07)
+    
+    let margins = self.view.safeAreaLayoutGuide
+    let scrollViewContentMargins = self.scrollView?.contentLayoutGuide
+    let scrollViewMargins = self.scrollView?.safeAreaLayoutGuide
+    
+    scrollView?.centerYAnchor.constraint(equalTo: margins.centerYAnchor).isActive = true
+    scrollView?.centerXAnchor.constraint(equalTo: margins.centerXAnchor).isActive = true
+    scrollView?.topAnchor.constraint(equalTo: margins.topAnchor).isActive = true
+    scrollView?.leftAnchor.constraint(equalTo: margins.leftAnchor).isActive = true
+    scrollView?.rightAnchor.constraint(equalTo: margins.rightAnchor).isActive = true
+    scrollView?.bottomAnchor.constraint(equalTo: margins.bottomAnchor).isActive = true
+    
+    pageControl?.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+    pageControl?.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+    pageControl?.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: -30).isActive = true
+    pageControl?.heightAnchor.constraint(equalToConstant: 20).isActive = true
+    
+    sampleImage01.centerYAnchor.constraint(equalTo: (scrollViewContentMargins?.centerYAnchor)!).isActive = true
+    sampleImage01.topAnchor.constraint(equalTo: (scrollViewMargins?.topAnchor)!).isActive = true
+    sampleImage01.leftAnchor.constraint(equalTo: (scrollViewContentMargins?.leftAnchor)!).isActive = true
+    sampleImage01.bottomAnchor.constraint(equalTo: (scrollViewMargins?.bottomAnchor)!).isActive = true
+    sampleImage01.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
+
+    sampleImage02.leftAnchor.constraint(equalTo: sampleImage01.safeAreaLayoutGuide.rightAnchor).isActive = true
+    sampleImage02.topAnchor.constraint(equalTo: (scrollViewMargins?.topAnchor)!).isActive = true
+    sampleImage02.bottomAnchor.constraint(equalTo: (scrollViewMargins?.bottomAnchor)!).isActive = true
+    sampleImage02.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
+
+    sampleImage03.leftAnchor.constraint(equalTo: sampleImage02.safeAreaLayoutGuide.rightAnchor).isActive = true
+    sampleImage03.topAnchor.constraint(equalTo: (scrollViewMargins?.topAnchor)!).isActive = true
+    sampleImage03.bottomAnchor.constraint(equalTo: (scrollViewMargins?.bottomAnchor)!).isActive = true
+    sampleImage03.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
+
+    sampleImage04.leftAnchor.constraint(equalTo: sampleImage03.safeAreaLayoutGuide.rightAnchor).isActive = true
+    sampleImage04.topAnchor.constraint(equalTo: (scrollViewMargins?.topAnchor)!).isActive = true
+    sampleImage04.bottomAnchor.constraint(equalTo: (scrollViewMargins?.bottomAnchor)!).isActive = true
+    sampleImage04.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
+
+    sampleImage05.leftAnchor.constraint(equalTo: sampleImage04.safeAreaLayoutGuide.rightAnchor).isActive = true
+    sampleImage05.topAnchor.constraint(equalTo: (scrollViewMargins?.topAnchor)!).isActive = true
+    sampleImage05.bottomAnchor.constraint(equalTo: (scrollViewMargins?.bottomAnchor)!).isActive = true
+    sampleImage05.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
+
+    sampleImage06.leftAnchor.constraint(equalTo: sampleImage05.safeAreaLayoutGuide.rightAnchor).isActive = true
+    sampleImage06.topAnchor.constraint(equalTo: (scrollViewMargins?.topAnchor)!).isActive = true
+    sampleImage06.bottomAnchor.constraint(equalTo: (scrollViewMargins?.bottomAnchor)!).isActive = true
+    sampleImage06.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
+
+    sampleImage07.leftAnchor.constraint(equalTo: sampleImage06.safeAreaLayoutGuide.rightAnchor).isActive = true
+    sampleImage07.topAnchor.constraint(equalTo: (scrollViewMargins?.topAnchor)!).isActive = true
+    sampleImage07.bottomAnchor.constraint(equalTo: (scrollViewMargins?.bottomAnchor)!).isActive = true
+    sampleImage07.widthAnchor.constraint(equalTo: margins.widthAnchor).isActive = true
+    sampleImage07.rightAnchor.constraint(equalTo: (scrollViewContentMargins?.rightAnchor)!).isActive = true
   }
   
 }
 
+extension ViewController: UIScrollViewDelegate {
+  
+  func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    let pageNumber = round(scrollView.contentOffset.x / scrollView.frame.size.width)
+    pageControl!.currentPage = Int(pageNumber)
+  }
+  
+}
